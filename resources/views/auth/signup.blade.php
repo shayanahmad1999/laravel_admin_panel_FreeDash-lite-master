c4ce6e979e087c7c933597bf7b47ff88eb9af0e0
shayanahmad1999 <shayanahmad235@gmail.com>
2024-02-12 18:57:38 +0500

    signup and login are  done

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -41,39 +41,37 @@
         <!-- ============================================================== -->
         <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative"
             style="background:url({{asset('assets/images/big/auth-bg.jpg')}}) no-repeat center center;">
-            <div class="auth-box row">
+            <div class="auth-box row text-center">
                 <div class="col-lg-7 col-md-5 modal-bg-img" style="background-image: url(../assets/images/big/3.jpg);">
                 </div>
                 <div class="col-lg-5 col-md-7 bg-white">
                     <div class="p-3">
-                        <div class="text-center">
-                            <img src="{{asset('assets/images/big/icon.png')}}" alt="wrapkit">
-                        </div>
-                        <h2 class="mt-3 text-center">Sign In</h2>
-                        <p class="text-center">Enter your email address and password to access admin panel.</p>
+                        <img src="{{asset('assets/images/big/icon.png')}}" alt="wrapkit">
+                        <h2 class="mt-3 text-center">Sign Up for Free</h2>
                         <form class="mt-4">
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="form-group mb-3">
-                                        <label class="form-label text-dark" for="uname">Username</label>
-                                        <input class="form-control" id="uname" type="text"
-                                            placeholder="enter your username">
+                                        <input class="form-control" type="text" placeholder="your name">
                                     </div>
                                 </div>
                                 <div class="col-lg-12">
                                     <div class="form-group mb-3">
-                                        <label class="form-label text-dark" for="pwd">Password</label>
-                                        <input class="form-control" id="pwd" type="password"
-                                            placeholder="enter your password">
+                                        <input class="form-control" type="email" placeholder="email address">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group mb-3">
+                                        <input class="form-control" type="password" placeholder="password">
                                     </div>
                                 </div>
                                 <div class="col-lg-12 text-center">
                                     <button type="submit" class="btn w-100 btn-dark">
-                                        <a href="{{route('admin.dashboard')}}" class="text text-light">Sign In</a>
+                                        <a href="{{route('admin.login')}}" class="text text-light">Sign Up</a>
                                     </button>
                                 </div>
                                 <div class="col-lg-12 text-center mt-5">
-                                    Don't have an account? <a href="{{route('admin.signup')}}" class="text-danger">Sign Up</a>
+                                    Already have an account? <a href="{{route('admin.login')}}" class="text-danger">Sign In</a>
                                 </div>
                             </div>
                         </form>
